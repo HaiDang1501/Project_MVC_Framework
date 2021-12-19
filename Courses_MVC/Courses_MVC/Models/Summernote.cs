eba9c78f736b02eff1d1a2feae1d3ddd90beac0b
HaiDang <19521319@gm.uit.edu.vn>
2021-12-20 00:15:07 +0700

    add summernote,

---
+++ Courses_MVC/Courses_MVC/Models/Summernote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses_MVC.Models
+{
+    public class Summernote
+    {
+        public Summernote(string IdEditor, bool LoadLibary = true)
+        {
+            idEditor = IdEditor;
+            loadLibary = LoadLibary;
+        }
+        public string idEditor { get; set; }
+
+        public bool loadLibary { get; set; }
+
+        public int height { get; set; } = 120;
+
+        public string toolBar { get; set; } = @"[
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['height', ['height']],
+            ['view', ['fullscreen', 'codeview', 'help']]
+        ]";
+    }
+}
