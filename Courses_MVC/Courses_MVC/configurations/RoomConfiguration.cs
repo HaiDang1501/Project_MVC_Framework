23ad6d84c7e0d97fdf354743375eee33b05c9064
HaiDang <19521319@gm.uit.edu.vn>
2021-12-31 08:38:46 +0700

    ...

---
+++ Courses_MVC/Courses_MVC/configurations/RoomConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Courses_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses_MVC.configurations
+{
+    public class RoomConfiguration : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.ToTable("Rooms");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            builder.Property(s => s.Name).IsRequired().HasMaxLength(100);
+
+            builder.HasOne(s => s.AppUser)
+                .WithMany(u => u.Rooms)
+                .IsRequired();
+        }
+    }
+}
